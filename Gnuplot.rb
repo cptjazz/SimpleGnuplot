d6d82506cbd3e592727d961a6149a63b010f1db0
Jazz <alexander@jesner.eu>
2011-11-20 13:16:13 +0100

    Converted tabs to spaces

--- Gnuplot.rb
+++ Gnuplot.rb
@@ -2,273 +2,273 @@ require 'narray'
 require './NArray_Extensions.rb'
 
 class Gnuplot
-	attr_accessor :synchronize
-	attr_accessor :debug
-	attr_accessor :replot
+  attr_accessor :synchronize
+  attr_accessor :debug
+  attr_accessor :replot
 
-	def initialize(gnuplot_path = "pgnuplot", dump_file = nil)
-		@gnuplot_path = gnuplot_path
+  def initialize(gnuplot_path = "pgnuplot", dump_file = nil)
+    @gnuplot_path = gnuplot_path
 
-		if dump_file
-			@stream = File.new(dump_file, "w")
-			@is_process = false
-		else
-			@stream = IO.popen(@gnuplot_path, "w")
-			@is_process = true
-			@stream.sync = true
-		end
+    if dump_file
+      @stream = File.new(dump_file, "w")
+      @is_process = false
+    else
+      @stream = IO.popen(@gnuplot_path, "w")
+      @is_process = true
+      @stream.sync = true
+    end
 
-		@temp_files = []
-		sleep 0.5
-	end
+    @temp_files = []
+    sleep 0.5
+  end
 
-	def Gnuplot.open(gnuplot_path = "gnuplot", &lambda)
-		g = Gnuplot.new(gnuplot_path)
+  def Gnuplot.open(gnuplot_path = "gnuplot", &lambda)
+    g = Gnuplot.new(gnuplot_path)
 
-		if block_given?
-			lambda.call(g)
-			g.close
-		end
+    if block_given?
+      lambda.call(g)
+      g.close
+    end
 
-		g
-	end
+    g
+  end
 
-	def Gnuplot.open_interactive(gnuplot_path = "pgnuplot", &lambda)
-		g = Gnuplot.new(gnuplot_path)
+  def Gnuplot.open_interactive(gnuplot_path = "pgnuplot", &lambda)
+    g = Gnuplot.new(gnuplot_path)
 
-		if block_given?
-			lambda.call(g)
-			g.close
-		end
+    if block_given?
+      lambda.call(g)
+      g.close
+    end
 
-		g
-	end
+    g
+  end
 
-	def Gnuplot.dump_script_to(filename, &lambda)
-		g = Gnuplot.new(nil, filename)
+  def Gnuplot.dump_script_to(filename, &lambda)
+    g = Gnuplot.new(nil, filename)
 
-		if block_given?
-			lambda.call(g)
-			g.close
-		end
+    if block_given?
+      lambda.call(g)
+      g.close
+    end
 
-		g
-	end
+    g
+  end
 
-	def load(script)
-		send(%Q{l "#{script}"})
-	end
+  def load(script)
+    send(%Q{l "#{script}"})
+  end
 
-	def terminal=(args)
-		if args.length == 2
-			send("se t #{args[0]} #{args[1]}")
-		else
-			send("se t #{args}")
-		end
-		
-		@replot_not_first_plot = false
-	end
+  def terminal=(args)
+    if args.length == 2
+      send("se t #{args[0]} #{args[1]}")
+    else
+      send("se t #{args}")
+    end
 
-	def output=(filename)
-		send(%Q{se out "#{filename}"})
-		@replot_not_first_plot = false
-	end
+    @replot_not_first_plot = false
+  end
 
-	def xlabel=(text)
-		send(%Q{se xl "#{text}"})
-	end
+  def output=(filename)
+    send(%Q{se out "#{filename}"})
+    @replot_not_first_plot = false
+  end
 
-	def ylabel=(text)
-		send(%Q{se yl "#{text}"})
-	end
+  def xlabel=(text)
+    send(%Q{se xl "#{text}"})
+  end
 
-	def y2label=(text)
-		send(%Q{se y2l "#{text}"})
-	end
+  def ylabel=(text)
+    send(%Q{se yl "#{text}"})
+  end
 
-	def ytics=(args)
-		send("se yti #{args[0]}, #{args[1]}")
-	end
+  def y2label=(text)
+    send(%Q{se y2l "#{text}"})
+  end
 
-	def y2tics=(args)
-		send("se y2ti #{args[0]}, #{args[1]}")
-		send("se yti nomi")
-	end
+  def ytics=(args)
+    send("se yti #{args[0]}, #{args[1]}")
+  end
 
-	def xrange=(args)
-		send "se xr [#{args[0]}:#{args[1]}]"
-	end
+  def y2tics=(args)
+    send("se y2ti #{args[0]}, #{args[1]}")
+    send("se yti nomi")
+  end
 
-	def yrange=(args)
-		send "se yr [#{args[0]}:#{args[1]}]"
-	end
+  def xrange=(args)
+    send "se xr [#{args[0]}:#{args[1]}]"
+  end
 
-	def y2range=(args)
-		send "se y2r [#{args[0]}:#{args[1]}]"
-	end
+  def yrange=(args)
+    send "se yr [#{args[0]}:#{args[1]}]"
+  end
 
-	def set_log(text)
-		send("se log #{text}")
-	end
+  def y2range=(args)
+    send "se y2r [#{args[0]}:#{args[1]}]"
+  end
 
-	def unset_log(text = "")
-		send("uns log #{text}")
-	end
+  def set_log(text)
+    send("se log #{text}")
+  end
 
-	def set(arg)
-		send("se #{arg}")
-	end
+  def unset_log(text = "")
+    send("uns log #{text}")
+  end
 
-	def unset(arg)
-		send("uns #{arg}")
-	end
+  def set(arg)
+    send("se #{arg}")
+  end
 
-	def title=(t)
-		send(%Q{se tit "#{t}"})
-	end
+  def unset(arg)
+    send("uns #{arg}")
+  end
 
-	def pause(l = -1)
-		send("pa #{l}")
-	end
+  def title=(t)
+    send(%Q{se tit "#{t}"})
+  end
 
-	def unset_output
-		send("uns out")
-		@stream.flush
-		@replot_not_first_plot = false
-	end
-	alias :flush :unset_output
+  def pause(l = -1)
+    send("pa #{l}")
+  end
 
-	def reset
-		send("reset")
-	end
+  def unset_output
+    send("uns out")
+    @stream.flush
+    @replot_not_first_plot = false
+  end
+  alias :flush :unset_output
 
-	def with_file(in_file)
-		@in_file = in_file
-		self
-	end
+  def reset
+    send("reset")
+  end
 
-	def with_data(data)
-		s = ""
+  def with_file(in_file)
+    @in_file = in_file
+    self
+  end
 
-		if data.is_a?(NArray)
-			if data.dim == 1
-				data.each do |x|
-					s << x.to_s << "\n"
-				end
-			else
-				s = data.to_gplot
-			end
-		else
-			s = data.to_s
-		end
+  def with_data(data)
+    s = ""
 
-		@temp_data = s
-		self
-	end
+    if data.is_a?(NArray)
+      if data.dim == 1
+        data.each do |x|
+          s << x.to_s << "\n"
+        end
+      else
+        s = data.to_gplot
+      end
+    else
+      s = data.to_s
+    end
 
-	def prepare_plot_command(args)
-		local_args = args.clone
+    @temp_data = s
+    self
+  end
 
-		if local_args.has_key? :title
-			local_args[:title] = %Q{"#{args[:title]}"}
-		end
+  def prepare_plot_command(args)
+    local_args = args.clone
 
-		if local_args.has_key? :f
-			local_args.delete :f
-		end
-		
-		a = local_args.inject("") { |res, elem| res << elem[0].to_s << " " << elem[1].to_s << " " }
-		a.gsub('title ""', 'notitle')
-	end
-	private :prepare_plot_command
+    if local_args.has_key? :title
+      local_args[:title] = %Q{"#{args[:title]}"}
+    end
 
-	def _plot_command
-		if @replot
-			cmd = @replot_not_first_plot ? 'rep' : 'p'
-			@replot_not_first_plot = true
-			
-			cmd
-		else
-			'p'
-		end
-	end
+    if local_args.has_key? :f
+      local_args.delete :f
+    end
 
-	def plot(args)
-		a = prepare_plot_command(args)
+    a = local_args.inject("") { |res, elem| res << elem[0].to_s << " " << elem[1].to_s << " " }
+    a.gsub('title ""', 'notitle')
+  end
+  private :prepare_plot_command
 
-		if args.has_key? :f
-			send(%Q{#{_plot_command} #{args[:f]} #{a}})
-		else
-			send(%Q{#{_plot_command} "#{@temp_data ? '-' : @in_file}" #{a}})
-		end
+  def _plot_command
+    if @replot
+      cmd = @replot_not_first_plot ? 'rep' : 'p'
+      @replot_not_first_plot = true
 
-		if @temp_data
-		  send(%Q{#{@temp_data}\ne\n})
-		  @temp_data = nil
-		end
-	end
+      cmd
+    else
+      'p'
+    end
+  end
 
-	def multiplot(arr)
-		a = arr.collect do |args|
-			prepare_plot_command args
-		end
+  def plot(args)
+    a = prepare_plot_command(args)
 
-		a = a.join(', "" ')
-		send(%Q{#{_plot_command} "#{@temp_data ? '-' : @in_file}" #{a}})
+    if args.has_key? :f
+      send(%Q{#{_plot_command} #{args[:f]} #{a}})
+    else
+      send(%Q{#{_plot_command} "#{@temp_data ? '-' : @in_file}" #{a}})
+    end
 
-		
-		arr.length.times do
-			if @temp_data
-			  send(%Q{#{@temp_data}\ne\n})
-			end
-		end
+    if @temp_data
+      send(%Q{#{@temp_data}\ne\n})
+      @temp_data = nil
+    end
+  end
 
-		@temp_data = nil
-	end
+  def multiplot(arr)
+    a = arr.collect do |args|
+      prepare_plot_command args
+    end
 
-	def splot(args)
-		a = prepare_plot_command(args)
-		send(%Q{sp "#{@temp_data ? '-' : @in_file}" #{a}})
+    a = a.join(', "" ')
+    send(%Q{#{_plot_command} "#{@temp_data ? '-' : @in_file}" #{a}})
 
-		if @is_process and @synchronize
-			send("pr 1")
-			tmp = @stream.readpartial(1)
-			puts tmp
-		end
-	end
 
-	def multisplot(arr)
-		a = arr.collect do |args|
-			prepare_plot_command args
-		end
+    arr.length.times do
+      if @temp_data
+        send(%Q{#{@temp_data}\ne\n})
+      end
+    end
 
-		a = a.join(%Q{, "#{@temp_data ? '-' : @in_file}" })
-		send(%Q{sp "#{@temp_data ? '-' : @in_file}" #{a}})
+    @temp_data = nil
+  end
 
-		arr.length.times do
-			if @temp_data
-			  send(%Q{#{@temp_data}\ne\n})
-			end
-		end
+  def splot(args)
+    a = prepare_plot_command(args)
+    send(%Q{sp "#{@temp_data ? '-' : @in_file}" #{a}})
 
-		@temp_data = nil
-	end
+    if @is_process and @synchronize
+      send("pr 1")
+      tmp = @stream.readpartial(1)
+      puts tmp
+    end
+  end
 
-	def replot
-		send("re")
-	end
+  def multisplot(arr)
+    a = arr.collect do |args|
+      prepare_plot_command args
+    end
 
-	def send(command)
-		@stream << (command << "\n")
-	end
-	private :send
+    a = a.join(%Q{, "#{@temp_data ? '-' : @in_file}" })
+    send(%Q{sp "#{@temp_data ? '-' : @in_file}" #{a}})
 
-	def hold
-		send "pa mouse"
-	end
+    arr.length.times do
+      if @temp_data
+        send(%Q{#{@temp_data}\ne\n})
+      end
+    end
 
-	def close
-		send("exit")
-		@stream.close
-	end
+    @temp_data = nil
+  end
+
+  def replot
+    send("re")
+  end
+
+  def send(command)
+    @stream << (command << "\n")
+  end
+  private :send
+
+  def hold
+    send "pa mouse"
+  end
+
+  def close
+    send("exit")
+    @stream.close
+  end
 end
--- NArray_Extensions.rb
+++ NArray_Extensions.rb
@@ -1,31 +1,31 @@
 require 'NArray'
 
 class NArray
-	def to_s
-		s = "["
-		self.each do |el|
-			s << el.to_s + ", "
-		end
-		s.chop!.chop!
-		s << "]"
+  def to_s
+    s = "["
+    self.each do |el|
+      s << el.to_s + ", "
+    end
+    s.chop!.chop!
+    s << "]"
 
-		s
-	end
+    s
+  end
 
-	def to_gplot
-		height = self.shape[1] || 1
-		width = self.shape[0]
-		s = ""
+  def to_gplot
+    height = self.shape[1] || 1
+    width = self.shape[0]
+    s = ""
 
-		height.times do |h|
-			offset = h*width
+    height.times do |h|
+      offset = h*width
 
-			width.times do |w|
-				s << self[offset + w].to_s + " "
-			end
-			s << "\n"
-		end
+      width.times do |w|
+        s << self[offset + w].to_s + " "
+      end
+      s << "\n"
+    end
 
-		s
-	end
+    s
+  end
 end
